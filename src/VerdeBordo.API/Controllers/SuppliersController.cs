3ebbabef0928272c7016b1e1ae10c25eb442b325
Pablo Souza <pablo.osouza@outlook.com>
2023-02-08 18:59:19 -0300

    feature: create SuppliersController with future features to implement

---
+++ src/VerdeBordo.API/Controllers/SuppliersController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VerdeBordo.API.Controllers
+{
+    [ApiController]
+    [Route("api/v1/suppliers")]
+    public class SuppliersController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetSuppliers()
+        {
+            return Ok();
+        }
+
+        [HttpGet("{supplierId:guid}")]
+        public async Task<IActionResult> GetSupplierById(Guid supplierId)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateSupplier()
+        {
+            return Ok();
+        }
+
+        [HttpPatch("{supplierId:guid}")]
+        public async Task<IActionResult> UpdateSupplier(Guid supplierId)
+        {
+            return Ok();
+        }
+
+        [HttpDelete("{supplierId:guid}")]
+        public async Task<IActionResult> DeleteSupplier(Guid supplierId)
+        {
+            return NoContent();
+        }
+
+        [HttpGet("{supplierId:guid}/products")]
+        public async Task<IActionResult> GetSupplierProducts(Guid productId)
+        {
+            return Ok();
+        }
+
+        [HttpGet("product/{productId:guid}")]
+        public async Task<IActionResult> GetProductById(Guid productId)
+        {
+            return Ok();
+        }
+    }
+}
