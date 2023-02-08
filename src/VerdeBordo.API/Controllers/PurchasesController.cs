e9c41d819ed1d7f17eceb5a5119f54255a7fca20
Pablo Souza <pablo.osouza@outlook.com>
2023-02-08 19:03:47 -0300

    feature: create PurchasesController with future features to implement

---
+++ src/VerdeBordo.API/Controllers/PurchasesController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VerdeBordo.API.Controllers
+{
+    [ApiController]
+    [Route("api/v1/purchases")]
+    public class PurchasesController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetPurchases()
+        {
+            return Ok();
+        }
+
+        [HttpGet("{purchaseId:guid}")]
+        public async Task<IActionResult> GetPurchaseById(Guid purchaseId)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreatePurchase()
+        {
+            return Ok();
+        }
+
+        [HttpPatch("{purchaseId:guid}")]
+        public async Task<IActionResult> UpdatePurchase(Guid purchaseId)
+        {
+            return Ok();
+        }
+
+        [HttpDelete("{purchaseId:guid}")]
+        public async Task<IActionResult> DeletePurchase(Guid purchaseId)
+        {
+            return NoContent();
+        }
+
+        [HttpPost("report")]
+        public async Task<IActionResult> GenerateReport()
+        {
+            return Ok();
+        }
+    }
+}
